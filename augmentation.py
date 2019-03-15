29a8314e6c02722aef917ae136a187ff2d1bd679
trangle1302 <35525072+trangle1302@users.noreply.github.com>
2019-03-15 12:10:55 +0000

    initial commit for augmentation using imgaug

---
+++ augmentation.py
@@ -0,0 +1,72 @@
+import os
+from glob import glob
+import cv2
+import numpy as np 
+import pandas as pd 
+from random import shuffle
+
+from imgaug import augmenters as iaa
+   
+def aug_seq():
+    sometimes = lambda aug: iaa.Sometimes(0.5, aug)
+    seq = iaa.Sequential(
+        [
+            # apply the following augmenters to most images
+            iaa.Fliplr(0.5), # horizontally flip 50% of all images
+            iaa.Flipud(0.2), # vertically flip 20% of all images
+            sometimes(iaa.Affine(
+                scale={"x": (0.9, 1.1), "y": (0.9, 1.1)}, # scale images to 80-120% of their size, individually per axis
+                translate_percent={"x": (-0.1, 0.1), "y": (-0.1, 0.1)}, # translate by -20 to +20 percent (per axis)
+                rotate=(-10, 10), # rotate by -45 to +45 degrees
+                shear=(-5, 5), # shear by -16 to +16 degrees
+                order=[0, 1], # use nearest neighbour or bilinear interpolation (fast)
+                cval=(0, 255), # if mode is constant, use a cval between 0 and 255
+                mode=ia.ALL # use any of scikit-image's warping modes (see 2nd image from the top for examples)
+            )),
+            iaa.SomeOf((0, 5),
+                [
+                    sometimes(iaa.Superpixels(p_replace=(0, 1.0), n_segments=(20, 200))), # convert images into their superpixel representation
+                    iaa.OneOf([
+                        iaa.GaussianBlur((0, 1.0)), # blur images with a sigma between 0 and 3.0
+                        iaa.AverageBlur(k=(3, 5)), # blur image using local means with kernel sizes between 2 and 7
+                        iaa.MedianBlur(k=(3, 5)), # blur image using local medians with kernel sizes between 2 and 7
+                    ]),
+                    iaa.Sharpen(alpha=(0, 1.0), lightness=(0.9, 1.1)),
+                    iaa.AdditiveGaussianNoise(loc=0, scale=(0.0, 0.01*255), per_channel=0.5), # add gaussian noise to images
+                    iaa.OneOf([
+                        iaa.Dropout((0.01, 0.05), per_channel=0.5), # randomly remove up to 10% of the pixels
+                        iaa.CoarseDropout((0.01, 0.03), size_percent=(0.01, 0.02), per_channel=0.2),
+                    ]),
+                    iaa.Invert(0.01, per_channel=True), # invert color channels
+                    iaa.Add((-2, 2), per_channel=0.5), 
+                    iaa.OneOf([
+                        iaa.Multiply((0.9, 1.1), per_channel=0.5),
+                        iaa.FrequencyNoiseAlpha(
+                            exponent=(-1, 0),
+                            first=iaa.Multiply((0.9, 1.1), per_channel=True),
+                            second=iaa.ContrastNormalization((0.9, 1.1))
+                        )
+                    ]),
+                    sometimes(iaa.ElasticTransformation(alpha=(0.5, 3.5), sigma=0.25)), # move pixels locally around (with random strengths)
+                    sometimes(iaa.PerspectiveTransform(scale=(0.01, 0.1)))
+                ],
+                random_order=True
+            )
+        ],
+        random_order=True
+    )
+    return seq
+
+def data_gen(list_files, id_label_map, batch_size, augment=False):
+    seq = aug_seq()
+    while True:
+        shuffle(list_files)
+        for batch in chunker(list_files, batch_size):
+            X = [cv2.imread(x) for x in batch]
+            Y = [id_label_map[get_id_from_file_path(x)] for x in batch]
+            if augment:
+                X = seq.augment_images(X)
+            X = [preprocess_input(x) for x in X]
+                
+            yield np.array(X), np.array(Y)
+            
