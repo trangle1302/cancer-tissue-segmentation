239e9d52260708e4a6fe61892a8b299ff46248bd
Reda94 <17529858+Reda94@users.noreply.github.com>
2019-05-05 23:20:14 +0100

    Add files via upload

---
+++ code/unit_tests/unit_tests_inceptionv3.py
@@ -0,0 +1,31 @@
+import mltest
+import kaggle_scripts_inceptionv3
+import tensorflow as tf
+import numpy as np
+
+def setup():
+  mltest.setup()
+
+def inceptionv3_mltest_suite():
+
+  input_tensor = tf.placeholder(tf.float32, (None, 100))
+  label_tensor = tf.placeholder(tf.int32, (None))
+
+  model = kaggle_scripts_inceptionv3.build_model(input_tensor, label_tensor)
+
+  feed_dict = {
+      input_tensor: np.random.normal(size=(10, 100)),
+      label_tensor: np.random.randint((100))
+  }
+
+  mltest.test_suite(
+      model.prediction,
+      model.train_op,
+      feed_dict=feed_dict)
+
+def test_range():
+  model = kaggle_scripts_inceptionv3.build_model()
+  mltest.test_suite(
+    model.logits,
+    model.train_op,
+    output_range=(0,1))
