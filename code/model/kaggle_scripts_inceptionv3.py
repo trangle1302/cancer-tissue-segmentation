134672c8b81b7135b3ce8617f98460b21a098d0b
Reda94 <17529858+Reda94@users.noreply.github.com>
2019-05-05 20:40:25 +0100

    Update kaggle_scripts_inceptionv3.py

--- code/model/kaggle_scripts_inceptionv3.py
+++ code/model/kaggle_scripts_inceptionv3.py
@@ -34,6 +34,9 @@ from albumentations import torch as AT
 import scipy.special
 from pytorchcv.model_provider import get_model as ptcv_get_model
 
+from cancer_dataset import CancerDataset 
+from data_transform import transformations 
+
 cudnn.benchmark = True
 
 # Seeding for reproducibility 
@@ -53,90 +56,9 @@ labels = pd.read_csv(base_dir+'train_labels.csv')
 tr, val = train_test_split(labels.label, stratify=labels.label, test_size=0.10, random_state=SEED)
 img_class_dict = {k:v for k, v in zip(labels.id, labels.label)}
 
-# Data loader module
-class CancerDataset(Dataset):
-    def __init__(self, datafolder, datatype='train', transform = transforms.Compose([transforms.CenterCrop(64),transforms.ToTensor()]), labels_dict={}):
-        self.datafolder = datafolder
-        self.datatype = datatype
-        self.image_files_list = [s for s in os.listdir(datafolder)]
-        self.transform = transform
-        self.labels_dict = labels_dict
-        if self.datatype == 'train':
-            self.labels = [labels_dict[i.split('.')[0]] for i in self.image_files_list]
-        else:
-            self.labels = [0 for _ in range(len(self.image_files_list))]
+# Data transformations:
 
-    def __len__(self):
-        return len(self.image_files_list)
-
-    def __getitem__(self, idx):
-        img_name = os.path.join(self.datafolder, self.image_files_list[idx])
-        img = cv2.imread(img_name)
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        image = self.transform(image=img)
-        image = image['image']
-
-        img_name_short = self.image_files_list[idx].split('.')[0]
-
-        if self.datatype == 'train':
-            label = self.labels_dict[img_name_short]
-        else:
-            label = 0
-        return image, label
-
-# Data augmentation by albumentations package
-# For detail of the available augmenters, check out https://github.com/albu/albumentations
-data_transforms = albumentations.Compose([
-    albumentations.Resize(224,224),
-    albumentations.RandomRotate90(p=0.5),
-    albumentations.Transpose(p=0.5),
-    albumentations.Flip(p=0.5),
-    albumentations.OneOf([
-        albumentations.CLAHE(clip_limit=2), albumentations.IAASharpen(), albumentations.IAAEmboss(), 
-        albumentations.RandomBrightness(), albumentations.RandomContrast(),
-        albumentations.JpegCompression(), albumentations.Blur(), albumentations.GaussNoise()], p=0.5), 
-    albumentations.HueSaturationValue(p=0.5), 
-    albumentations.ShiftScaleRotate(shift_limit=0.15, scale_limit=0.15, rotate_limit=45, p=0.5),
-    albumentations.Normalize(mean=[0.485, 0.456, 0.406],std=[0.229, 0.224, 0.225]),
-    AT.ToTensor()
-    ])
-
-data_transforms_test = albumentations.Compose([
-    albumentations.Resize(224,224),
-    albumentations.Normalize(mean=[0.485, 0.456, 0.406],std=[0.229, 0.224, 0.225]),
-    AT.ToTensor()
-    ])
-
-data_transforms_tta0 = albumentations.Compose([
-    albumentations.Resize(224,224),
-    albumentations.RandomRotate90(p=0.5),
-    albumentations.Transpose(p=0.5),
-    albumentations.Flip(p=0.5),
-    albumentations.Normalize(mean=[0.485, 0.456, 0.406],std=[0.229, 0.224, 0.225]),
-    AT.ToTensor()
-    ])
-
-data_transforms_tta1 = albumentations.Compose([
-    albumentations.Resize(224,224),
-    albumentations.RandomRotate90(p=1),
-    albumentations.Normalize(mean=[0.485, 0.456, 0.406],std=[0.229, 0.224, 0.225]),
-    AT.ToTensor()
-    ])
-
-data_transforms_tta2 = albumentations.Compose([
-    albumentations.Resize(224,224),
-    albumentations.Transpose(p=1),
-    albumentations.Normalize(mean=[0.485, 0.456, 0.406],std=[0.229, 0.224, 0.225]),
-    AT.ToTensor()
-    ])
-
-data_transforms_tta3 = albumentations.Compose([
-    albumentations.Resize(224,224),
-    albumentations.Flip(p=1),
-    albumentations.Normalize(mean=[0.485, 0.456, 0.406],std=[0.229, 0.224, 0.225]),
-    AT.ToTensor()
-    ])
-
+data_transforms, data_transforms_test, data_transforms_tta0, data_transforms_tta1, data_transforms_tta2, data_transforms_tta3 = transformations()
 
 # Input image path to the dataloader to prepare for training
 dataset = CancerDataset(datafolder=base_dir+'train/', datatype='train', transform=data_transforms, labels_dict=img_class_dict)
