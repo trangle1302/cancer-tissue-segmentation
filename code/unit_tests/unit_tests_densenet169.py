1c62384e3639319fe9cf88a5068738fb395538e1
Reda94 <17529858+Reda94@users.noreply.github.com>
2019-05-10 08:41:32 +0100

    Update unit_tests_densenet169.py

--- code/unit_tests/unit_tests_densenet169.py
+++ code/unit_tests/unit_tests_densenet169.py
@@ -1,35 +1,32 @@
 import mltest
-import kaggle_scripts_densenet169
 import tensorflow as tf
 import numpy as np
+import random
+from kaggle_scripts_densenet169 import Densenet169 
+from cancer_dataset import CancerDataset
 
 def setup():
   mltest.setup()
 
-dataset = CancerDataset(datafolder=base_dir+'train/', datatype='train', transform=data_transforms, labels_dict=img_class_dict)
-train_sampler = SubsetRandomSampler(list(tr.index)) 
-batch_size = 24
-num_workers = 0
-train_loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, sampler=train_sampler, num_workers=num_workers)
-
-
-from my_model import Densenet169
-
 def densenet169_mltest_suite():
   
+  dataset = CancerDataset(datafolder=base_dir+'train/', datatype='train', transform=data_transforms, labels_dict=img_class_dict)
+  train_sampler = SubsetRandomSampler(list(tr.index)) 
+  batch_size = 24
+  num_workers = 0
+  train_loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, sampler=train_sampler, num_workers=num_workers)
   (data, target) = train_loader[1]
   #data, target = data.cuda(), target.cuda() #if testing in CPU, remove this line
   model_conv = DenseNet169(pretrained=True).cuda()
   model_conv.train()
   output = model_conv(data)
+  random.seed(123)
+  feed_dict = {
+      input_tensor: np.random.normal(size=(10, 100)),
+      label_tensor: np.random.randint((100))
+  }
+  setup()
   mltest.test_suite(
       data,
       output,
       feed_dict=feed_dict)
-
-def test_range():
-  model = kaggle_scripts_densenet169.build_model()
-  mltest.test_suite(
-    model.logits, #output (= model_conv(data)) should be between 0,1
-    model.train_op,
-    output_range=(0,1))
